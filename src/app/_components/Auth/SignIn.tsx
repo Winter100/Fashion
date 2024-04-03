68ed175c96d321deb3eb50086dcf689fd0720a78
winter100 <100taeng@gmail.com>
2024-04-03 21:11:25 +0900

    feat: 로그인 및 회원가입 페이지 커스텀 훅 적용

--- src/app/_components/Auth/SignIn.tsx
+++ src/app/_components/Auth/SignIn.tsx
@@ -1,18 +1,53 @@
 "use client";
 
-import { Button, Label, TextInput } from "flowbite-react";
+import useFetch from "@/app/_hooks/useFetch";
+import useInput from "@/app/_hooks/useInput";
+import { Button, Label, Spinner, TextInput } from "flowbite-react";
+import { FormEvent } from "react";
+
+const fetchUrl = "";
+const method = "POST";
 
 export default function SignIn() {
+  const { input, handleChange } = useInput({
+    email: "",
+    password: "",
+  });
+
+  const { isLoading, handleFetch } = useFetch();
+
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    const { email, password } = input;
+
+    if (!email || !password) return;
+
+    const value = {
+      email,
+      password,
+    };
+    const data = await handleFetch(fetchUrl, method, value);
+  }
+
   return (
-    <form className="m-auto mt-40 flex w-full max-w-md flex-col gap-4 ">
+    <form
+      onSubmit={handleSubmit}
+      className="m-auto mt-40 flex w-full max-w-md flex-col gap-4 "
+    >
+      <h1 className="m-auto text-8xl">로그인</h1>
       <div>
         <div className="mb-2 block ">
-          <Label className="text-2xl" htmlFor="email1" value="이메일" />
+          <Label className="text-2xl" htmlFor="email" value="이메일" />
         </div>
         <TextInput
           style={{ fontSize: "24px", padding: "7px" }}
-          id="email1"
+          onChange={handleChange}
+          name="email"
+          value={input.email}
+          id="email"
           type="email"
+          disabled={isLoading}
           required
         />
       </div>
@@ -21,14 +56,22 @@ export default function SignIn() {
           <Label className="text-2xl" htmlFor="password1" value="비밀번호" />
         </div>
         <TextInput
+          onChange={handleChange}
           style={{ fontSize: "24px", padding: "7px" }}
-          id="password1"
+          name="password"
+          value={input.password}
+          id="password"
           type="password"
+          disabled={isLoading}
           required
         />
       </div>
-      <Button type="submit">
-        <span className=" text-2xl">로그인</span>
+      <Button type="submit" disabled={isLoading}>
+        {!isLoading ? (
+          <span className=" text-2xl">로그인</span>
+        ) : (
+          <Spinner aria-label="Spinner button example" size="sm" />
+        )}
       </Button>
     </form>
   );
--- src/app/_components/Auth/SignUp.tsx
+++ src/app/_components/Auth/SignUp.tsx
@@ -1,16 +1,52 @@
-import { Button, Label, TextInput } from "flowbite-react";
+"use client";
+import useFetch from "@/app/_hooks/useFetch";
+import useInput from "@/app/_hooks/useInput";
+import { Button, Label, Spinner, TextInput } from "flowbite-react";
+import { FormEvent } from "react";
+
+const fetchUrl = "";
+const method = "POST";
 
 export default function SignUp() {
+  const { input, handleChange } = useInput({
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { isLoading, handleFetch } = useFetch();
+
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    const { email, password } = input;
+
+    if (!email || !password) return;
+
+    const value = {
+      email,
+      password,
+    };
+    const data = await handleFetch(fetchUrl, method, value);
+  }
   return (
-    <form className="m-auto mt-40 flex w-full max-w-md flex-col gap-4 ">
+    <form
+      onSubmit={handleSubmit}
+      className="m-auto mt-40 flex w-full max-w-md flex-col gap-4 "
+    >
+      <h1 className="m-auto text-8xl">회원가입</h1>
       <div>
         <div className="mb-2 block ">
           <Label className="text-2xl" htmlFor="email1" value="이메일" />
         </div>
         <TextInput
           style={{ fontSize: "24px", padding: "7px" }}
-          id="email1"
+          onChange={handleChange}
+          name="email"
+          value={input.email}
+          id="email"
           type="email"
+          disabled={isLoading}
           required
         />
       </div>
@@ -20,8 +56,12 @@ export default function SignUp() {
         </div>
         <TextInput
           style={{ fontSize: "24px", padding: "7px" }}
-          id="password1"
+          onChange={handleChange}
+          name="password"
+          value={input.password}
+          id="password"
           type="password"
+          disabled={isLoading}
           required
         />
       </div>
@@ -35,13 +75,21 @@ export default function SignUp() {
         </div>
         <TextInput
           style={{ fontSize: "24px", padding: "7px" }}
+          onChange={handleChange}
+          name="password2"
+          value={input.password2}
           id="password2"
           type="password"
+          disabled={isLoading}
           required
         />
       </div>
       <Button type="submit">
-        <span className=" text-2xl">회원가입</span>
+        {!isLoading ? (
+          <span className=" text-2xl">회원가입</span>
+        ) : (
+          <Spinner aria-label="Spinner button example" size="sm" />
+        )}
       </Button>
     </form>
   );
