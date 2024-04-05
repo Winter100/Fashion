b47183350281c3633716830de114b6b219322eb2
winter100 <100taeng@gmail.com>
2024-04-05 19:31:41 +0900

    feat: 로그인 및 회원가입 페이지 추가

---
+++ src/app/(route)/auth/signin/page.tsx
@@ -0,0 +1,5 @@
+import SignIn from "@/app/_components/Auth/SignIn";
+
+export default function page() {
+  return <SignIn />;
+}
---
+++ src/app/(route)/auth/signup/page.tsx
@@ -0,0 +1,5 @@
+import SignUp from "@/app/_components/Auth/SignUp";
+
+export default function page() {
+  return <SignUp />;
+}
